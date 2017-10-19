3f085b8a9de9dd0c1ba60acc88eee7276a9f9dae
YYAustin <1415853402@qq.com>
2017-10-19 11:34:05 +0800

    first code

--- app/src/main/java/com/example/administrator/myproject/LoginActivity.java
+++ app/src/main/java/com/example/administrator/myproject/LoginActivity.java
@@ -4,10 +4,15 @@ package com.example.administrator.myproject;
  * Created by Administrator on 2017/10/18.
  */
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import static com.example.administrator.myproject.R.id.button1;
 
 /**
  * Created by Administrator on 2017/10/16.
@@ -18,5 +23,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ActionBar actionbar = getSupportActionBar();
+        if(actionbar != null) {
+            actionbar.hide();
+        }
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, FirstActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/administrator/myproject/MainActivity.java
+++ app/src/main/java/com/example/administrator/myproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.myproject;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionbar = getSupportActionBar();
+        if(actionbar != null){
+            actionbar.hide();
+        }
     }
 }
